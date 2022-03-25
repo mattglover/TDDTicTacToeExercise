e09e14886911509cd68d822a2fc6dce125f44ceb
Ryan Hill <ryan.hill@skybettingandgaming.com>
2022-03-25 13:07:11 +0000

    Checkpoint commit
    Testing that a player can now win a game horizontally and diagonally

--- Sources/TicTacToeKit/TTTGameManager.swift
+++ Sources/TicTacToeKit/TTTGameManager.swift
@@ -11,6 +11,17 @@ enum SquareState {
     case vacant
 }
 
+enum EndGameStatus {
+    case win
+    case stalemate
+}
+
+struct PlacementOutcome {
+    var wasValid: Bool
+    var gridRepresentation: String
+    var endGame: EndGameStatus?
+}
+
 class TTTGameManager {
     
     private(set) var gridSize: Int!
@@ -28,8 +39,69 @@ class TTTGameManager {
         self.currentPlayer = player
     }
     
-    func place(player: Player, x: Int, y: Int) {
-        squares[0] = .occupiedPlayerOne
+    func place(player: Player, x: Int, y: Int) -> PlacementOutcome {
+        guard currentPlayer == player else {
+            return placementOutcome(wasSuccessful: false)
+        }
+
+        guard isWithinBoundsOfArray(x: x, y: y, gridSize: gridSize) else {
+            return placementOutcome(wasSuccessful: false)
+        }
+
+        let index = (x * gridSize) + y
+        guard squares[index] == SquareState.vacant else {
+            return placementOutcome(wasSuccessful: false)
+        }
+
+        let occupiedPlayer = player == .player1 ? SquareState.occupiedPlayerOne : SquareState.occupiedPlayerTwo
+        squares[index] = occupiedPlayer
+
+        let endGame = calculateEndGame()
+
+        switch endGame {
+        case .win:
+            return placementOutcome(wasSuccessful: true, endGame: .win)
+        case .stalemate:
+            return placementOutcome(wasSuccessful: true, endGame: .stalemate)
+        case .none:
+            switchCurrentPlayer()
+            return placementOutcome(wasSuccessful: true)
+        }
+    }
+
+    private func placementOutcome(wasSuccessful success: Bool, endGame: EndGameStatus? = nil) -> PlacementOutcome {
+        return PlacementOutcome(wasValid: success, gridRepresentation: gridRepresentationString, endGame: endGame)
+    }
+
+    private func isWithinBoundsOfArray(x: Int, y: Int, gridSize: Int) -> Bool {
+        return x >= 0 && x < gridSize && y >= 0 && y < gridSize
+    }
+
+    private func calculateEndGame() -> EndGameStatus? {
+        if hasRowWin() || hasDiagonalWin() {
+            return .win
+        }
+        return nil
+    }
+
+    private func hasRowWin() -> Bool {
+        return gridRepresentationString.contains("XXX") || gridRepresentationString.contains("OOO")
+    }
+
+    private func hasDiagonalWin() -> Bool {
+        return squares[0] == .occupiedPlayerOne && squares[4] == .occupiedPlayerOne && squares[8] == .occupiedPlayerOne ||
+        squares[2] == .occupiedPlayerOne && squares[4] == .occupiedPlayerOne && squares[6] == .occupiedPlayerOne
+    }
+
+    func switchCurrentPlayer() {
+        switch currentPlayer {
+        case .player1:
+            self.currentPlayer = .player2
+        case .player2:
+            self.currentPlayer = .player1
+        case .none:
+            break
+        }
     }
     
     var gridRepresentationString: String {
@@ -45,7 +117,7 @@ class TTTGameManager {
             } else if squareState == .occupiedPlayerOne {
                 string += "X"
             } else if squareState == .occupiedPlayerTwo {
-                string += "0"
+                string += "O"
             }
         }
         
--- Tests/TicTacToeKitTests/TTTGameManagerTests.swift
+++ Tests/TicTacToeKitTests/TTTGameManagerTests.swift
@@ -39,23 +39,154 @@ class TTTGameManagerTests: XCTestCase {
         XCTAssertEqual(sut.currentPlayer, .player2)
     }
     
-    //MARK: - Placing Token
+    //MARK: - Making first plays
     func testFirstPlay_Player1MakesMove_0_0() {
         sut = Helper.defaultSUT()
         let player: Player = .player1
         sut.startGame(player: player)
         
-        sut.place(player: player, x: 0, y: 0)
+        let outcome = sut.place(player: player, x: 0, y: 0)
         
         XCTAssertEqual(sut.gridRepresentationString, Constants.topLeftX_3x3)
+        XCTAssertEqual(outcome.gridRepresentation, Constants.topLeftX_3x3)
+        XCTAssertTrue(outcome.wasValid)
+    }
+
+    func testFirstPlay_Player1MakesMove_0_0_SwitchesCurrentPlayerToPlayer2() {
+        sut = Helper.defaultSUT()
+        let player: Player = .player1
+        sut.startGame(player: player)
+        let _ = sut.place(player: player, x: 0, y: 0)
+
+        let currentPlayer = sut.currentPlayer
+
+        XCTAssertEqual(currentPlayer, .player2)
+    }
+
+    func testFirstPlay_Player1MakesMoveOutsideOfGrid() {
+        sut = Helper.defaultSUT()
+        let player: Player = .player1
+        sut.startGame(player: player)
+
+        let testCoordinates: [(x: Int, y: Int)] = [
+            (-1, 0),
+            (0, -1),
+            (-1, -1),
+            (3, 0),
+            (0, 3),
+            (3, 3),
+        ]
+
+        for testCoordinate in testCoordinates {
+            let outcome = sut.place(player: player, x: testCoordinate.x, y: testCoordinate.y)
+            XCTAssertFalse(outcome.wasValid)
+            XCTAssertEqual(sut.currentPlayer, .player1)
+        }
+
+    }
+
+    //MARK: - Making second plays
+    func testSecondPlay_Player2MakesMove_0_1() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player1)
+        let _ = sut.place(player: .player1, x: 0, y: 0)
+
+        let outcome = sut.place(player: .player2, x: 0, y: 1)
+
+        XCTAssertEqual(outcome.gridRepresentation, Constants.XO_3x3)
+    }
+
+    func testSecondPlay_Player2MakeMove_OccupiedSquare() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player1)
+        let _ = sut.place(player: .player1, x: 0, y: 0)
+
+        let outcome = sut.place(player: .player2, x: 0, y: 0)
+
+        XCTAssertFalse(outcome.wasValid)
+        XCTAssertEqual(outcome.gridRepresentation, Constants.topLeftX_3x3)
+    }
+
+    func testSecondPlay_PlayerOneFollowsPlayer1() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player1)
+        let _ = sut.place(player: .player1, x: 0, y: 0)
+
+        let outcome = sut.place(player: .player1, x: 0, y: 1)
+
+        XCTAssertFalse(outcome.wasValid)
+        XCTAssertEqual(outcome.gridRepresentation, Constants.topLeftX_3x3)
+    }
+
+    //MARK: - Winning Plays
+
+    func testPlayerOneWinsGame_RowWin() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player1)
+        let _ = sut.place(player: .player1, x: 0, y: 0)
+        let _ = sut.place(player: .player2, x: 2, y: 2)
+        let _ = sut.place(player: .player1, x: 0, y: 1)
+        let _ = sut.place(player: .player2, x: 2, y: 1)
+
+        let outcome = sut.place(player: .player1, x: 0, y: 2)
+
+        XCTAssertTrue(outcome.wasValid)
+        XCTAssertEqual(outcome.endGame, .win)
+        XCTAssertEqual(outcome.gridRepresentation, Constants.topRowX_Win)
+    }
+
+    func testPlayerTwoWinsGame_RowWin() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player2)
+        let _ = sut.place(player: .player2, x: 0, y: 0)
+        let _ = sut.place(player: .player1, x: 2, y: 2)
+        let _ = sut.place(player: .player2, x: 0, y: 1)
+        let _ = sut.place(player: .player1, x: 2, y: 1)
+
+        let outcome = sut.place(player: .player2, x: 0, y: 2)
+
+        XCTAssertEqual(outcome.endGame, .win)
+    }
+
+    func testPlayerOneWinsGame_DiagonalWin() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player1)
+        let _ = sut.place(player: .player1, x: 0, y: 0)
+        let _ = sut.place(player: .player2, x: 0, y: 1)
+        let _ = sut.place(player: .player1, x: 1, y: 1)
+        let _ = sut.place(player: .player2, x: 0, y: 2)
+
+        let outcome = sut.place(player: .player1, x: 2, y: 2)
+
+        XCTAssertTrue(outcome.wasValid)
+        XCTAssertEqual(outcome.endGame, .win)
+        XCTAssertEqual(outcome.gridRepresentation, Constants.X_DiagonalWin)
+    }
+
+    func testPlayerTwoWinsGame_DiagonalWin() {
+        sut = Helper.defaultSUT()
+        sut.startGame(player: .player2)
+        let _ = sut.place(player: .player2, x: 0, y: 0)
+        let _ = sut.place(player: .player1, x: 0, y: 1)
+        let _ = sut.place(player: .player2, x: 1, y: 1)
+        let _ = sut.place(player: .player1, x: 0, y: 2)
+
+        let outcome = sut.place(player: .player2, x: 2, y: 2)
+
+        XCTAssertEqual(outcome.endGame, .win)
     }
     
+}
+
+extension TTTGameManagerTests {
     struct Constants {
         static let empty_3x3 = "~~~\n~~~\n~~~"
-//        static let centredO_3x3 = "~~~\n~o~\n~~~"
         static let topLeftX_3x3 = "X~~\n~~~\n~~~"
+        static let XO_3x3 = "XO~\n~~~\n~~~"
+        static let topRowX_Win = "XXX\n~~~\n~OO"
+        static let X_DiagonalWin = "XOO\n~X~\n~~X"
     }
-    
+
     struct Helper {
         static func defaultSUT() -> TTTGameManager {
             return TTTGameManager(gridSize: 3)
